4de48302f72e40227b025a700d515b905ee0cdde
Bernd Porr <berndporr@f2s.com>
2016-09-24 09:05:43 +0100

    Switches on logging via adb. All debug logging in AttysComm is now disabled by default as it should be. The others will follow.

---
+++ logging.sh
@@ -0,0 +1,7 @@
+~/Android/Sdk/platform-tools/adb shell setprop log.tag.ScalarInputSpec VERBOSE
+~/Android/Sdk/platform-tools/adb shell setprop log.tag.SIDiscoverer VERBOSE
+~/Android/Sdk/platform-tools/adb shell setprop log.tag.ScalarSensor VERBOSE
+~/Android/Sdk/platform-tools/adb shell setprop log.tag.SensorAppearance VERBOSE
+~/Android/Sdk/platform-tools/adb shell setprop log.tag.MainActivity VERBOSE
+~/Android/Sdk/platform-tools/adb shell setprop log.tag.AttysPlot VERBOSE
+~/Android/Sdk/platform-tools/adb shell setprop log.tag.AttysComm VERBOSE
